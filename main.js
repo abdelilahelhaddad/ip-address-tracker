5080428325eff2a3562a63551558e6f2e5100835
Abdelilah EL Haddad <elhaddadabdelilah@gmail.com>
2021-06-04 23:55:38 +0100

    :fire: :art: Get Data from API :sparkles: :globe_with_meridians:

--- main.js
+++ main.js
@@ -15,7 +15,6 @@ arrow.addEventListener("click", () => {
   getGeolocation();
 })
 
-
 function getGeolocation() {
   geolocation.getGeolocation()
     .then(results => {
@@ -25,7 +24,7 @@ function getGeolocation() {
 }
 
 //Setting up the map
-var map = L.map('map').setView([lat(), lng()], 13);
+var map = L.map('map').setView([51.605, -0.09], 13);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
